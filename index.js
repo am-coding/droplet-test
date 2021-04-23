3e7a9ba556ebbee52b2f871fbec79f137a04f98d
am-coding <amrikdeveloper@gmail.com>
2021-04-24 00:03:34 +0530

    made changes to the script

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get("/", function (req, res) {
       ignoreDefaultArgs: ["--disable-extensions"],
       args: ["--no-sandbox"],
       headless: false,
+      executablePath: "/usr/bin/chromium-browser",
     })
     .then(async function (browser) {
       const page = await browser.newPage();
