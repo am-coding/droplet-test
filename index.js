854001474b963583eecde5f72f32293223222990
am-coding <amrikdeveloper@gmail.com>
2021-04-23 23:58:05 +0530

    made changes to puppeteer

--- index.js
+++ index.js
@@ -5,27 +5,33 @@ const puppeteer = require("puppeteer"); // Adding Puppeteer
 // Wrapping the Puppeteer browser logic in a GET request
 app.get("/", function (req, res) {
   // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
-  puppeteer.launch().then(async function (browser) {
-    const page = await browser.newPage();
-    await page.goto("http://digidb.io/digimon-list/");
+  puppeteer
+    .launch({
+      ignoreDefaultArgs: ["--disable-extensions"],
+      args: ["--no-sandbox"],
+      headless: false,
+    })
+    .then(async function (browser) {
+      const page = await browser.newPage();
+      await page.goto("http://digidb.io/digimon-list/");
 
-    // Targeting the DOM Nodes that contain the Digimon names
-    const digimonNames = await page.$$eval(
-      "#digiList tbody tr td:nth-child(2) a",
-      function (digimons) {
-        // Mapping each Digimon name to an array
-        return digimons.map(function (digimon) {
-          return digimon.innerText;
-        });
-      }
-    );
+      // Targeting the DOM Nodes that contain the Digimon names
+      const digimonNames = await page.$$eval(
+        "#digiList tbody tr td:nth-child(2) a",
+        function (digimons) {
+          // Mapping each Digimon name to an array
+          return digimons.map(function (digimon) {
+            return digimon.innerText;
+          });
+        }
+      );
 
-    // Closing the Puppeteer controlled headless browser
-    await browser.close();
+      // Closing the Puppeteer controlled headless browser
+      await browser.close();
 
-    // Sending the Digimon names to Postman
-    res.send(digimonNames);
-  });
+      // Sending the Digimon names to Postman
+      res.send(digimonNames);
+    });
 });
 
 // Making Express listen on port 8000
